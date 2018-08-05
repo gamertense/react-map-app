4027c9e6a31d872c397407c4a588304dec41ad1f
Antonio Jack <gamertense@live.com>
2018-08-06 00:10:39 +0700

    Successfully draw a polygon.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker, Polyline, Polygon } from 'react-native-maps';
 
 
 export default class App extends Component {
@@ -10,54 +10,105 @@ export default class App extends Component {
         id: Math.random().toString(36).substr(2, 9),
         title: 'Bangkok',
         latlng: {
-          latitude: 13.736717,
-          longitude: 100.523186,
+          latitude: 37.8025259,
+          longitude: -122.4351431,
         },
         description: 'description'
       }
+    ],
+    polygons: [
+      {
+        latitude: 13.736717,
+        longitude: 100.523186,
+      },
+      {
+        latitude: 15.736717,
+        longitude: 105.523186,
+      }
+    ],
+    coordinates: [
+      [
+        [
+          -122.43455886840819,
+          37.804358908571395
+        ],
+        [
+          -122.4326705932617,
+          37.7897092979573
+        ],
+        [
+          -122.41155624389647,
+          37.790116270812874
+        ],
+        [
+          -122.40829467773436,
+          37.79717011082383
+        ],
+        [
+          -122.43455886840819,
+          37.804358908571395
+        ]
+      ]
     ]
   }
 
   onPress(e) {
-    console.log(e.nativeEvent)
+    // console.log(e.nativeEvent)
     const lat = e.nativeEvent.coordinate.latitude;
     const long = e.nativeEvent.coordinate.longitude;
-    this.setState({
-      markers: [
-        ...this.state.markers,
-        {
-          id: Math.random().toString(36).substr(2, 9),
-          title: 'Bangkok' + Math.random().toString(36).substr(2, 9),
-          latlng: {
-            latitude: lat,
-            longitude: long,
-          },
-          description: 'description'
-        }
-      ]
-    })
+    markersCopy = [...this.state.markers];
+    markersCopy.push(
+      {
+        id: Math.random().toString(36).substr(2, 9),
+        title: 'Bangkok' + Math.random().toString(36).substr(2, 9),
+        latlng: {
+          latitude: lat,
+          longitude: long,
+        },
+        description: 'description'
+      }
+    )
+    polygonsCopy = [...this.state.polygons];
+    polygonsCopy.push([long, lat])
+    this.setState({ markers: markersCopy })
+    // this.setState({ polygons: polygonsCopy })
   }
 
   render() {
+    const initlat = 37.8025259;
+    const initlong = -122.4351431;
+    const polygons = this.state.coordinates[0].map(coordsArr => {
+      let coords = {
+        latitude: coordsArr[1],
+        longitude: coordsArr[0],
+      }
+      return coords;
+    })
+    console.log(polygons)
     return (
       <View style={styles.container}>
         <MapView style={styles.map}
           initialRegion={{
-            latitude: 13.736717,
-            longitude: 100.523186,
+            latitude: initlat,
+            longitude: initlong,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
           onPress={e => this.onPress(e)}
         >
           {this.state.markers.map(marker => (
-            <MapView.Marker
+            <Marker
               key={marker.id}
               coordinate={marker.latlng}
               title={marker.title}
               description={marker.description}
             />
           ))}
+          <Polygon
+            coordinates={polygons}
+            fillColor="rgba(0, 200, 0, 0.5)"
+            strokeColor="rgba(0,0,0,0.5)"
+            strokeWidth={2} />
         </MapView>
       </View >
     );
