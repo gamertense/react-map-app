1c1ade24adfbff612aeaed523cf8fdeec7ca6067
Antonio Jack <gamertense@live.com>
2018-09-04 19:55:45 +0700

    Add space between ไร่ งาน วา area message

--- App.js
+++ App.js
@@ -77,7 +77,7 @@ export default class App extends React.Component {
 
       return (
         <View style={styles.area_message} >
-          <Text> {areaInRai + ' ไร่' + areaInNgarn + ' งาน' + areaInWa + ' ตารางวา'}</Text>
+          <Text> {areaInRai + ' ไร่ ' + areaInNgarn + ' งาน ' + areaInWa + ' ตารางวา '}</Text>
         </View>
       )
     }
